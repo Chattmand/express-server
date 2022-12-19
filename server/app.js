81d3e6ec7bb27cbb71f69ea4bba5194d88517242
DamarChattman <damarchattman@gmail.com>
2022-12-19 12:20:56 -0800

    changed root route to return status of 200

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // import files and packages up here
-module.exports = app;
-var express = require('data.json');
+
+var topSpots = require('./data.json');
+var express = require('express');
 
 // create your express server below
 var app;
@@ -9,17 +10,18 @@ var PORT = 3000;
 
 // add your routes and middleware below
 
-app.get('/hello', (req, res)=>{
+app.get('/', (req, res)=>{
 	res.set('Content-Type', 'text/html');
 	res.status(200).send("<h1>'It's Workin'</h1>");
 });
 
 // finally export the express application
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("Server is Successfully Running, and App is"
-					,listening ,on ,port + PORT)
-	else
-		console.log("Error occurred, server can't start", error);
-	}
-);
+// app.listen(PORT, (error) =>{
+// 	if(!error)
+// 		console.log("Server is Successfully Running, and App is" +
+// 					"listening on port" + PORT)
+// 	else
+// 		console.log("Error occurred, server can't start", error);
+// 	}
+//);
+module.exports = app;
