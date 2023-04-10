9b510f697f3c75305355c60108edba0ca9c583ef
DamarChattman <damarchattman@gmail.com>
2023-04-10 11:32:39 -0700

    final commit(clean-up)

--- server/app.js
+++ server/app.js
@@ -1,30 +1,21 @@
-// import files and packages up here
-
 var topSpots = require('./data.json');
 var express = require('express');
 
-// create your express server below
+
 var app;
 var app = express();
 var PORT = 3000;
 
-// add your routes and middleware below
+
 
 app.get('/', (req, res)=>{
 	res.set('Content-Type', 'text/html');
 	res.status(200).send("<h1>'It's Workin'</h1>");
 });
 
-//finally export the express application
+
 app.get('/data', function(req, res){
     res.send(topSpots)
 })
-// app.listen(PORT, (error) =>{
-// 	if(!error)
-// 		console.log("Server is Successfully Running, and App is" +
-// 					"listening on port" + PORT)
-// 	else
-// 		console.log("Error occurred, server can't start", error);
-// 	}
-// );
+
 module.exports = app;
